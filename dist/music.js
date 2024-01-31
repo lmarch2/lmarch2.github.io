9c0ae284a449359f670205d4393735816fafc4f5
lmarch2 <2524158037@qq.com>
2024-01-31 18:45:14 +0800

    Site updated: 2024-01-31 18:45:13

--- dist/music.js
+++ dist/music.js
@@ -3,11 +3,11 @@ const ap = new APlayer({
     fixed: true,
     autoplay: true,
     audio: [
-	{
+	    {
         name: '违背的青春',      #歌名
         artist: '薛之谦',  #歌手
         url: 'https://music.163.com/#/song?id=1307334195',    #mp3的url地址
-        cover: 'cover.jpg' #歌的封面
+        cover: 'cover.jpg', #歌的封面
       }
-     ]
+    ]
 });
